243d035cf7655576bc7f079abfb170ab4d5552f2
schrags08 <schrags08@hotmail.com>
2013-12-30 23:50:15 -0600

    Rename Room model, while I mess around with EF6 and git...

---
+++ experiments/eftesting/eftesting/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eftesting.Models
+{
+    /// <summary>
+    /// Model for the room - codefirst 
+    /// - all the properties should be dynamically ORM'd from the db
+    /// - assume a local sql server or sql compact
+    /// 
+    /// </summary>
+    /// 
+    public class Room
+    {
+        
+    }
+}
