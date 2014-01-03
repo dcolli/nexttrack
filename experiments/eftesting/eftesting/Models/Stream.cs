f8b25d6965def1e1fd8504ffa29438139a1173b6
schrags08 <schrags08@hotmail.com>
2014-01-03 11:34:57 -0600

    create model classses

--- experiments/eftesting/eftesting/Models/Stream.cs
+++ experiments/eftesting/eftesting/Models/Stream.cs
@@ -3,27 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace eftesting.Models
 {
-    /// <summary>
-    /// Model for the room - codefirst 
-    /// - all the properties should be dynamically ORM'd from the db
-    /// - assume a local sql server or sql compact
-    /// 
-    /// </summary>
-    /// 
-    public class Room
+    public class Stream
     {
-        public int RoomId { get; set; }
-        public string Name { get; set; }        
+        public int SteamId { get; set; }
+        public string Name { get; set; }
         
         //TODO: how can we implement these DateTime fields with default values instead of having to set them manually?
         public DateTime DateCreated { get; set; }
-        public DateTime DateLastModified { get; set; }
-
+        
         [Timestamp]
         public Byte[] Timestamp { get; set; } //should we be doing this or is it obsolete?
-
-        public virtual ICollection<Stream> Streams { get; set; }
     }
 }
